0790563fed8d66b87b4514e966c1d093a7c81017
Chandra Kishan <sandishsingha21@gmail.com>
2024-06-17 22:21:21 +0530

    added the font-size

--- src/components/body.js
+++ src/components/body.js
@@ -1,31 +1,43 @@
-import React from 'react';
-import './body.css';
+import React from "react";
+import "./body.css";
 
 const BodyComponent = () => {
-    return (
-        <div className="body-container">
-            <div className="content">
-                <br></br>
-                <br></br>
-                <br></br>
-                <h4 className="subtitle"><img src="./dot.png" alt="Dot" className="dot-image" />          Future of Care</h4>
-                <h1 className="title">A Sustainable Vision</h1>
-                <h1 className="title">for Ambulance</h1>
-                <h1 className="title">Services</h1>
-                <br/>
-                <p className="description">"Join Our Vision for Enhanced Emergency Care: Embracing Futuristic Solutions<br></br>
-                for Ambulance Fleet Management, Patient Care, and Service Optimization."</p>
-            </div>
-
-            <div className="images">
-                <img src="./ambulance.jpeg" alt="Ambulance" className="ambulance-image" />
-                <img src="./doctor.jpeg" alt="Doctor" className="doctor-image" />
-            </div>
-            <div className="button-container">
-                <button className="contact-button">Get in touch</button>
-            </div>
+  return (
+    <div className="body-container">
+      <div className="content">
+        <br></br>
+        <br></br>
+        <br></br>
+        <h4 className="subtitle">
+          <img src="./dot.png" alt="Dot" className="dot-image" /> Future of Care
+        </h4>
+        <div className="text">
+          <h1 className="title">A Sustainable Vision</h1>
+          <h1 className="title">for Ambulance</h1>
+          <h1 className="title">Services</h1>
         </div>
-    );
+        <br />
+        <p className="description">
+          "Join Our Vision for Enhanced Emergency Care: Embracing Futuristic
+          Solutions<br></br>
+          for Ambulance Fleet Management, Patient Care, and Service
+          Optimization."
+        </p>
+      </div>
+
+      <div className="images">
+        <img
+          src="./ambulance.jpeg"
+          alt="Ambulance"
+          className="ambulance-image"
+        />
+        <img src="./doctor.jpeg" alt="Doctor" className="doctor-image" />
+      </div>
+      <div className="button-container">
+        <button className="contact-button">Get in touch</button>
+      </div>
+    </div>
+  );
 };
 
 export default BodyComponent;
